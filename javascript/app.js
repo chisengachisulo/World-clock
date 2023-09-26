90a82d79f0f945c7cdef0089f81219e4ba44a357
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-09-26 15:32:53 +0100

    added the single location time zone

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  //berlin
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  let berlinTime = moment().tz("Europe/Berlin");
+  berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+  berlinTimeElement.innerHTML = berlinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  //lagos
+  let lagosElement = document.querySelector("#lagos");
+  let lagosDateElement = lagosElement.querySelector(".date");
+  let lagosTimeElement = lagosElement.querySelector(".time");
+  let lagosTime = moment().tz("Africa/Lagos");
+  lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
